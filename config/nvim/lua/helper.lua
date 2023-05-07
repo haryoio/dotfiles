0b732af22f87241d08102cfb97f866e170d06108
haryoiro <mizusecocolte@gmail.com>
2023-05-07 19:46:34 +0900

    👍 keymap用ヘルパーを定義

--- config/nvim/lua/helper.lua
+++ config/nvim/lua/helper.lua
@@ -1,3 +1,4 @@
+local M = {}
 
 function import(module_name)
   local ok, module = pcall(require, module_name)
@@ -8,4 +9,24 @@ function import(module_name)
   end
 end
 
+M.keybind = function()
+	map = vim.keymap.set
 
+	function nmap(key, action, option)
+		map("n", key, action, option)
+	end
+
+	function imap(key, action, option)
+		map("i", key, action, option)
+	end
+
+	function tmap(key, action, option)
+		map("t", key, action, option)
+	end
+
+	function vmap(key, action, option)
+		map("v", key, action, option)
+	end
+end
+
+return M
