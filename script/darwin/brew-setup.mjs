6798406335ebd21e842f0dee9945cc070cc687d8
haryoiro <mizusecocolte@gmail.com>
2023-05-07 19:35:05 +0900

    👍 インストーラがすべて動くように記述
    	renamed:    install.sh -> install-remote.sh
    	new file:   script/darwin/asdf-setup.sh
    	new file:   script/darwin/brew-setup.mjs
    	deleted:    script/darwin/install-asdf.sh
    	deleted:    script/darwin/install-brew.sh
    	new file:   script/darwin/install.sh
    	deleted:    script/darwin/link.sh

---
+++ script/darwin/brew-setup.mjs
@@ -0,0 +1,78 @@
+#!/usr/bin/env zx
+const { execSync } = require("child_process");
+const { readdirSync, statSync } = require("fs");
+const path = require("path");
+
+const formulas = [
+  "bat",
+  "exa",
+  "starship",
+  "peco",
+  "ripgrep",
+  "tldr",
+  "tig",
+  "lazygit",
+  "wezterm",
+  "neovim",
+  "efm-langserver",
+  "vifm",
+  "gh",
+  "ghq",
+  "slack",
+  "visual-studio-code",
+  "koekeishiya/formulae/skhd",
+  "koekeishiya/formulae/yabai",
+  "sheldon",
+  "asdf",
+  "mas",
+  "font-sauce-code-pro-nerd-font",
+];
+
+const dotfilesDir = path.join(
+  process.env.HOME,
+  ".local",
+  "share",
+  "dotfiles",
+  "config"
+);
+const dotconfigDir = path.join(process.env.HOME, ".config");
+
+await $`brew tap homebrew/cask-fonts`;
+
+function installBrewPackages() {
+  for (const pkg of formulas) {
+    try {
+      execSync(`brew install ${pkg}`);
+    } catch (error) {
+      console.log(
+        `${pkg}のインストールに失敗しました。アップデートを試みます。`
+      );
+      continue;
+    }
+  }
+}
+
+function isDirectory(path) {
+  try {
+    return statSync(path).isDirectory();
+  } catch (error) {
+    return false;
+  }
+}
+
+function linkingConfigFiles() {
+  const configFiles = readdirSync(dotfilesDir);
+  for (const pkg of configFiles) {
+    const path = `${dotfilesDir}/${pkg}`;
+    if (isDirectory(path)) {
+      console.log(`${path} -> ${dotconfigDir}`);
+      execSync(`ln -sf ${path} ${dotconfigDir}`);
+    } else {
+      console.log(`${path} -> $HOME`);
+      execSync(`ln -sf ${path} $HOME`);
+    }
+  }
+}
+
+installBrewPackages();
+linkingConfigFiles();
--- script/darwin/install-asdf.sh
+++
@@ -1,5 +0,0 @@
-
-
-
-asdf plugin-add java https://github.com/halcyon/asdf-java.git
-
