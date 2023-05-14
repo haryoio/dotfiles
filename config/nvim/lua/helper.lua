226aa23c23610e3fee3461a55dade36e4db99cda
haryoiro <mizusecocolte@gmail.com>
2023-05-15 08:43:44 +0900

    ✨ neotreeを追加

--- config/nvim/lua/helper.lua
+++ config/nvim/lua/helper.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function import(module_name)
-  local ok, module = pcall(require, module_name)
-  if ok then
-    return module
-  else
-    return nil
-  end
+	local ok, module = pcall(require, module_name)
+	if ok then
+		return module
+	else
+		return nil
+	end
 end
 
 M.keybind = function()
